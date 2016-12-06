ced57395d1436fb14eddfa1f63a49d96d2bac3ca
justlive <qq11419041@163.com>
2016-12-06 14:41:11 +0800

    add chat client

---
+++ ChatClient/src/main/java/cn/my/chat/model/ServerData.java
@@ -0,0 +1,51 @@
+package cn.my.chat.model;
+
+import lombok.Data;
+
+/**
+ * 服务端消息发送基本结构
+ * @author WB
+ *
+ */
+
+@Data
+public class ServerData {
+
+	/**
+	 * 版本号
+	 */
+	private String version;
+	/**
+	 * 返回code 00000：成功 其他错误
+	 */
+	private String code = "00000";
+	/**
+	 * 操作
+	 */
+	private String option;
+	/**
+	 * 错误信息
+	 */
+	private String msg;
+	/**
+	 * 消息正文
+	 */
+	private String content;
+	
+	public ServerData(){}
+	
+	public ServerData(String version, String code, String msg, String content) {
+		super();
+		this.version = version;
+		this.code = code;
+		this.msg = msg;
+		this.content = content;
+	}
+	
+	public ServerData(String version, String code, String msg) {
+		super();
+		this.version = version;
+		this.code = code;
+		this.msg = msg;
+	}
+}
---
+++ ChatClient/src/main/java/cn/my/chat/model/User.java
@@ -0,0 +1,26 @@
+package cn.my.chat.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 用户信息
+ * @author WB
+ *
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	/**
+	 * 用户名
+	 */
+	private String name;
+	/**
+	 * 密码
+	 */
+	private String password;
+
+}
