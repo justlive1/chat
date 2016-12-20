f38fbd156a698f26507aea81b530a934ebcb758c
justlive1 <qq11419041@163.com>
2016-12-20 11:29:41 +0800

    add yml configuration

--- ChatClient/src/main/java/cn/my/chatclient/Application.java
+++ ChatClient/src/main/java/cn/my/chatclient/Application.java
@@ -2,11 +2,9 @@ package cn.my.chatclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = { "cn.my.chatclient" })
-@PropertySource("config/config.properties")
 @EnableAsync
 public class Application {
 
